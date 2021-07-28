0ef8f2631337918828001f3517ef9adacb113351
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-07-28 21:32:12 +0300

    making 3 colomns

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import './App.css';
-import data from './components/assets/data.json';
+
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import SelectedBeast from './components/SelectedBeast';
+
 
 
 class App extends React.Component {
@@ -16,9 +16,7 @@ class App extends React.Component {
       <div>
 
         <Header />
-        <Main 
-		data = {data} // send data to the main Component 
-		 SelectedBeast = {SelectedBeast}/>
+        <Main />
         <Footer />
         
       </div>
--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -3,38 +3,43 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import {Row, Col} from 'react-bootstrap';
 
 class HornedBeasts extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      numberOfVotes: 0
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			numberOfVotes: 0
+		};
+	}
 
 	vote = () => {
-	  this.setState({
-	    numberOfVotes: this.state.numberOfVotes += 1
-	  });
+		this.setState({
+			numberOfVotes: this.state.numberOfVotes += 1
+		});
 	}
 
 	render() {
-	  return (
-	    <div>
+		return (
+			<div>
+				<Row>
+					<Col>
+						<Card style={{ width: '18rem' }}>
+							<Card.Img onClick={this.vote} variant="top" src={this.props.image_url} />
+							<Card.Body>
+								<Card.Title>{this.props.title}</Card.Title>
+								<Card.Text>
+									❤️{this.props.description} {this.state.numberOfVotes}
+								</Card.Text>
 
-	      <Card style={{ width: '18rem' }}>
-	        <Card.Img onClick={this.vote} variant="top" src={this.props.image_url} />
-	        <Card.Body>
-	          <Card.Title>{this.props.title}</Card.Title>
-	          <Card.Text>
-							❤️{this.props.description} {this.state.numberOfVotes}
-	          </Card.Text>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
 
-	        </Card.Body>
-	      </Card>
-	    </div>
-	  );
+			</div>
+		);
 	}
 }
 
