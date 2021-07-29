bd6602b3c3d201df0eb5dc0f0c89bba925955040
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-07-29 03:01:02 +0300

    switching the branches

--- src/App.js
+++ src/App.js
@@ -6,17 +6,52 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-
+import Data from './components/assets/data.json';
+import SelectedBeast from './components/SelectedBeast'
 
 
 class App extends React.Component {
+
+	constructor (props){
+		super(props);
+		this.state = {
+			shown: false,
+			title: null,
+			description: null, 
+			image_url: null
+		};
+	}
+	modalData = (title, description, image_url) => {
+		this.setState({
+			title: title,
+			description: description,
+			image_url: image_url
+		})
+	}
+
+	UpdateTheModel = () => {
+		this.setState ({
+			shown: !this.state.shown
+		})
+	}
+	
 
   render() {
     return (
       <div>
 
         <Header />
-        <Main />
+		<SelectedBeast
+		shown = {this.state.shown}
+		title = {this.state.title}
+		description = {this.state.description}
+		image_url = {this.state.image_url}
+		updateModel = {updateTheModel()}
+		 />
+        <Main
+		Data={Data} 
+		updateModel = {updateTheModel()}
+		/>
         <Footer />
         
       </div>
